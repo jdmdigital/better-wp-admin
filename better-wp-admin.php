a3be2df194e8c4cf25cf7e5483fcd1a3ff702c51
JDM Digital <jdmdigital@users.noreply.github.com>
2016-08-25 10:59:15 -0500

    Create better-wp-admin.php

---
+++ better-wp-admin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Plugin Name: Better WP Admin
+ * Plugin URI: https://github.com/jdmdigital/better-wp-admin/
+ * Description: A WordPress Custom Admin Plugin for customizing the WordPress backend to be more intuitive and easier to use for casual users (e.g. better). - Upload and Activate.
+ * Version: 1.2
+ * Author: JDM Digital
+ * Author URI: http://jdmdigital.co
+ * License: GPLv2 or later
+ * GitHub Plugin URI: https://github.com/jdmdigital/better-wp-admin/
+ * GitHub Branch: master
+*/
+
+if( !function_exists('better_wp_admin_style') ) {
+  function better_wp_admin_style() {
+    wp_enqueue_style('better-wp-admin', plugins_url('better-wp-admin.css', __FILE__));
+  }
+}
+add_action('admin_enqueue_scripts', 'better_wp_admin_style');
+add_action('login_enqueue_scripts', 'better_wp_admin_style');
+
+?>
